b89f456cf7410c07d7e8d4de5c48bf09e2b340cd
Malo457 <malostamourbilodeau@gmail.com>
2020-09-21 14:58:36 -0400

    Exercices

--- exercice.py
+++ exercice.py
@@ -8,14 +8,50 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	sousTotal = 0
+	for elem in data:
+		sousTotal += elem[INDEX_QUANTITY]*elem[INDEX_PRICE]
+	taxes = sousTotal * 0.15
+	total = sousTotal + taxes
+	reponse = name
+	#{ce qu'on veut afficher : combien de caractère.quelle précision
+	reponse += '\n' + f'SOUS TOTAL {sousTotal : >10.2f} $'
+	reponse += '\n' + f'TAXES      {taxes : >10.2f} $'
+	reponse += '\n' + f'TOTAL      {total : >10.2f} $'
+
+	return reponse
 
 
 def format_number(number, num_decimal_digits):
-	return ""
+	entier = abs(int(number))
+	decimal = abs(number) % 1.0
+	ch_decimal = '.' + str(int(round(decimal * 10**num_decimal_digits)))
+	chaineFinale = ch_decimal
+
+	while entier >= 1000:
+		digits = entier % 1000
+		chaine = f" {digits:0>3}"
+		chaineFinale = chaine + chaineFinale
+		entier = entier // 1000
+
+	chaineFinale = str(entier) + chaineFinale
+	return ('-' if number < 0 else '') + chaineFinale
 
 def get_triangle(num_rows):
-	return ""
+	carBordure = '+'
+	carTriangle = 'A'
+	largeurTri = (num_rows - 1) * 2 + 1
+	ligneBordure = carBordure * (largeurTri + 2)
+	resultat = ligneBordure
+	i = 0
+	while i < num_rows:
+		nombreCarac = 1 + i * 2
+		caracTri = nombreCarac * carTriangle
+		ligneTriangle = f'{caracTri: ^{largeurTri}}'
+		resultat += '\n' + carBordure + ligneTriangle + carBordure
+		i += 1
+	resultat += '\n' + ligneBordure
+	return resultat
 
 
 if __name__ == "__main__":
